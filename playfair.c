d862cbd376c5790d88c906f9f25644575319778c
Isaac <isaac.yeo.linux@gmail.com>
2020-08-14 17:01:32 +0800

    Use getopt and command-line flags

--- playfair.c
+++ playfair.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <ctype.h>
 #include <errno.h>
+#include <unistd.h>
 
 #define TOERR(m) fprintf(stderr, m)
 
@@ -14,23 +15,9 @@ struct pair {
 
 static bool odd = false;
 static struct pair reftable[25];
+static short int table_w = 5, table_h = 5;
 #ifndef MAX_MSG_LEN
 #error Please specify -DMAX_MSG_LEN=XXX
-#endif
-
-#if defined ENCRYPT
-#define COMPUTE encrypt
-#elif defined DECRYPT
-#define COMPUTE decrypt
-#else
-#error Please specify -DENCYPT or -DDECRYPT
-#endif
-
-#ifndef TABLE_WIDTH
-#error Please specify -DTABLE_WIDTH
-#endif
-#ifndef TABLE_HEIGHT
-#error Please specify -DTABLE_HEIGHT
 #endif
 
 void encrypt(char **result, char **table, struct pair firstletter, struct pair secondletter);
@@ -38,60 +25,113 @@ void decrypt(char **result, char **table, struct pair firstletter, struct pair s
 
 int main(int argc, char **argv)
 {
-	/* Init/error handling */
-	/* Allocate memory */
-	char *ans = calloc(MAX_MSG_LEN, sizeof(char));
-	char **table = malloc(TABLE_HEIGHT * sizeof(char*));
-	for (int i = 0; i < TABLE_HEIGHT; ++i)
-		table[i] = malloc(TABLE_WIDTH * sizeof(char));
+	FILE *tablefile;
+	char *tablefile_name;
+	bool encode = true;
+	char *msg;
 
-	switch (argc) {
-		case 1:
-			TOERR("[ERROR] No message specified.  Bailing out!\n");
-			goto free_mem;
+	bool tflag = false;
+	bool edflag = false;
+	bool wflag = false;
+	bool hflag = true;
+
+	extern int optind;
+	extern char *optarg;
+	char c;
+
+	/* Parsing options */
+	while ((c = getopt(argc, argv, "t:edwh")) != -1) {
+		switch (c) {
+		case 't':
+			if (tflag)
+				TOERR("[WARN] -t flag specified more than once.\n");
+			tflag = true;
+			tablefile_name = optarg;
 			break;
-		case 2:
-			TOERR("[ERROR] No tablefile specified.  Bailing out!\n");
-			goto free_mem;
+
+		case 'e':
+			if (edflag)
+				TOERR("[WARN] -e or -d flag(s) specified more than once.\n");
+			edflag = true;
+			encode = true;
 			break;
-		case 3:
-			;;
-			FILE *tablefile = fopen(argv[2], "r");
-			if (errno != 0) {
-				TOERR("[ERROR] Keyfile not found.  Bailing out!\n");
-				goto free_mem;
-			}
-			printf("[NOTE] Using tablefile %s\n",  argv[2]);
-			for (int i = 0; i < TABLE_HEIGHT; i++) {
-				for (int j = 0; j < TABLE_WIDTH; j++)
-					table[i][j] = toupper(fgetc(tablefile));
-				fgetc(tablefile); /* Skip newline */
-			}
+
+		case 'd':
+			if (edflag)
+				TOERR("[WARN] -e or -d flag(s) specified more than once.\n");
+			edflag = true;
+			encode = false;
 			break;
-		default:
-			TOERR("Too many arguments.  Bailing out!\n");
-			goto free_mem;
+
+		case 'w':
+			if (wflag)
+				TOERR("[WARN] -w flag specified more than once.\n");
+			table_w = atoi(optarg);
 			break;
+
+		case 'h':
+			if (hflag)
+				TOERR("[WARN] -h flag specified more than once.\n");
+			table_h = atoi(optarg);
+			break;
+		}
 	}
 
-	if (strlen(argv[1]) % 2) {
+	if (optind >= argc) {
+		TOERR("[ERROR] No message specified.  Bailing out!\n");
+		exit(1);
+	} else {
+		msg = argv[optind];
+	}
+
+	if (!edflag) {
+		TOERR("[ERROR] One of -e (encode) or -d (decode) flags not specified.  Bailing out!\n");
+		exit(1);
+	}
+
+	char **table = malloc(table_h * sizeof(char*));
+	for (int i = 0; i < table_h; ++i)
+		table[i] = malloc(table_w * sizeof(char));
+
+	if (!tflag) {
+		TOERR("[ERROR] No tablefile specified.  Bailing out!\n");
+		exit(1);
+	}
+	if (access(optarg, F_OK) != -1) {
+		TOERR("[ERROR] File not found.  Bailing out!\n");
+		exit(2);
+	} else {
+		printf("[NOTE] Using tablefile %s\n",  tablefile_name);
+		tablefile = fopen(tablefile_name, "r");
+		for (int i = 0; i < table_h; i++) {
+			for (int j = 0; j < table_w; j++)
+				table[i][j] = toupper(fgetc(tablefile));
+			fgetc(tablefile); /* Skip newline */
+		}
+	}
+	/* Done parsing options */
+	char *ans = calloc(strlen(msg)+1, sizeof(char));
+
+	if (strlen(msg) % 2) {
 		TOERR("[WARN] Message length is odd.  Appending a random letter.\n");
 		odd = true;
 	}
 
-	for (int i = 0; i < TABLE_HEIGHT; i++) {
-		for (int j = 0; j < TABLE_WIDTH; j++)
+	for (int i = 0; i < table_h; i++) {
+		for (int j = 0; j < table_w; j++)
 			reftable[table[i][j]-'A'] = (struct pair){i, j};
 	}
 
-	for (size_t i = 0; i < strlen(argv[1]); i += 2)
-		COMPUTE(&ans, table, reftable[toupper(argv[1][i])-'A'], reftable[toupper(argv[1][i+1])-'A']);
-
+	for (size_t i = 0; i < strlen(msg); i += 2) {
+		if (encode)
+			encrypt(&ans, table, reftable[toupper(msg[i])-'A'], reftable[toupper(msg[i+1])-'A']);
+		else
+			decrypt(&ans, table, reftable[toupper(msg[i])-'A'], reftable[toupper(msg[i+1])-'A']);
+	}
 
 	printf("%s\n", ans);
 
-free_mem:
-	for (int i = 0; i < TABLE_HEIGHT; i++)
+	for (int i = 0; i < table_h; i++)
 		free(table[i]);
 	free(table);
 	free(ans);
@@ -102,23 +142,23 @@ free_mem:
 void encrypt(char **result, char **table, struct pair firstletter, struct pair secondletter)
 {
 	if (firstletter.first == secondletter.first) { /* Same row */
-		if (firstletter.second == TABLE_WIDTH-1) /* Wrap */
+		if (firstletter.second == table_w-1) /* Wrap */
 			strcat(*result, (char[2]){table[firstletter.first][0], '\0'});
 		else
 			strcat(*result, (char[2]){table[firstletter.first]
 					[firstletter.second+1], '\0'});
-		if (secondletter.second == TABLE_WIDTH-1) /* Wrap */
+		if (secondletter.second == table_w-1) /* Wrap */
 			strcat(*result, (char[2]){table[secondletter.first][0], '\0'});
 		else
 			strcat(*result, (char[2]){table[secondletter.first]
 					[secondletter.second+1], '\0'});
 	} else if (firstletter.second == secondletter.second) { /* Same column */
-		if (firstletter.first == TABLE_HEIGHT-1) /* Wrap */
+		if (firstletter.first == table_h-1) /* Wrap */
 			strcat(*result, (char[2]){table[0][firstletter.second], '\0'});
 		else
 			strcat(*result, (char[2]){table[firstletter.first+1]
 					[firstletter.second], '\0'});
-		if (secondletter.first == TABLE_HEIGHT-1) /* Wrap */
+		if (secondletter.first == table_h-1) /* Wrap */
 			strcat(*result, (char[2]){table[0][secondletter.second], '\0'});
 		else
 			strcat(*result, (char[2]){table[secondletter.first+1]
@@ -136,23 +176,23 @@ void decrypt(char **result, char **table, struct pair firstletter, struct pair s
 {
 	if (firstletter.first == secondletter.first) { /* Same row */
 		if (firstletter.second == 0) /* Wrap */
-			strcat(*result, (char[2]){table[firstletter.first][TABLE_WIDTH-1], '\0'});
+			strcat(*result, (char[2]){table[firstletter.first][table_w-1], '\0'});
 		else
 			strcat(*result, (char[2]){table[firstletter.first]
 					[firstletter.second-1], '\0'});
 		if (secondletter.second == 0) /* Wrap */
-			strcat(*result, (char[2]){table[secondletter.first][TABLE_WIDTH-1], '\0'});
+			strcat(*result, (char[2]){table[secondletter.first][table_w-1], '\0'});
 		else
 			strcat(*result, (char[2]){table [secondletter.first]
 					[secondletter.second-1], '\0'});
 	} else if (firstletter.second == secondletter.second) { /* Same column */
 		if (firstletter.first == 0) /* Wrap */
-			strcat(*result, (char[2]){table[TABLE_HEIGHT-1][firstletter.second], '\0'});
+			strcat(*result, (char[2]){table[table_h-1][firstletter.second], '\0'});
 		else
 			strcat(*result, (char[2]){table[firstletter.first-1]
 					[firstletter.second], '\0'});
 		if (secondletter.first == 5-1) /* Wrap */
-			strcat(*result, (char[2]){table[TABLE_HEIGHT-1][secondletter.second], '\0'});
+			strcat(*result, (char[2]){table[table_h-1][secondletter.second], '\0'});
 		else
 			strcat(*result, (char[2]){table[secondletter.first-1]
 					[secondletter.second], '\0'});
